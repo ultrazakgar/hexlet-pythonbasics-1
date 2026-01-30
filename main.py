7a5dbfb5f4d8cb41b09b353a94ecd1bdb61db000
ultrazakgar <zahariavinogr@gmail.com>
2026-01-30 11:10:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -78,8 +78,42 @@ def get_odd_count(text): # четных чисел
             counter += 1
     return(counter)
 
-print(get_odd_count("2468"))
-print(get_odd_count("13579"))
-print(get_odd_count("01234567"))
+# print(get_odd_count("2468"))
+# print(get_odd_count("13579"))
+# print(get_odd_count("01234567"))
 
+# Пишем функцию для управления умным домом
 
+# Написать функцию check_access которая на основе булевых параметров:
+#     has_keycard (bool): Наличие действующей ключ-карты.
+#     has_fingerprint (bool): Совпадение отпечатка пальца.
+#     is_alarm (bool): Сработала ли сигнализация (True - тревога!).
+#     is_daylight (bool): Дневное ли время (True - день).
+
+# Возвращает. Решение об открытии двери (True - открыть, False - не открывать).
+
+# Правила доступа:
+# Дверь можно открыть ключом-картой ИЛИ с помощью отпечатка пальца.
+# Однако, даже при наличии ключа или отпечатка, дверь НЕ откроется, если сработала сигнализация (например, при взломе).
+
+# Для дополнительной безопасности, если используется ключ-карта, 
+# система требует, чтобы в этот момент был дневной свет (имитация работы датчика освещенности). 
+# Это правило НЕ применяется к отпечатку пальца.
+
+def check_access(has_keycard: bool,has_fingerprint: bool,is_alarm: bool, is_daylight: bool):
+    if is_alarm:
+        return(False)
+    if has_fingerprint:
+        return(True)
+    if has_keycard and is_daylight:
+        return(True)
+        
+    return(False)
+
+# Примеры работы функции (тесты)
+# print("Тест 1 (Ключ, день, нет тревоги):", check_access(True, False, False, True))   # Ожидается: True
+# print("Тест 2 (Ключ, ночь, нет тревоги):", check_access(True, False, False, False))  # Ожидается: False (для ключа нужен день)
+# print("Тест 3 (Палец, ночь, нет тревоги):", check_access(False, True, False, False)) # Ожидается: True (для пальца день не нужен)
+# print("Тест 4 (Палец, день, но ЕСТЬ тревога):", check_access(False, True, True, True)) # Ожидается: False (тревога блокирует все)
+# print("Тест 5 (Ключ И палец, день, нет тревоги):", check_access(True, True, False, True)) # Ожидается: True
+# print("Тест 6 (Ничего нет):", check_access(False, False, False, True))               # Ожидается: False
